bbeb73b1bdb18f6260de4b1cdf60d7810f0d0a71
Matthew Gomez Cullen <mattgc1282@hotmail.co.uk>
2025-03-24 17:59:09 +0000

    Update model to cumulative sum

--- quant_num_generator/diqrng/circuits.py
+++ quant_num_generator/diqrng/circuits.py
@@ -1,70 +1,106 @@
+from collections import Counter
 import math
-import numpy as np
 from qiskit import QuantumCircuit, generate_preset_pass_manager
 from qiskit.quantum_info import SparsePauliOp
 from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2, EstimatorV2
 from qiskit_aer import AerSimulator
 
-simulated = 'FakeBrisbane' # Change to simulate other QPUs
+simulated = 'FakeKyiv' # Change to simulate other QPUs
 dynamic_fake_provider = __import__('qiskit_ibm_runtime.fake_provider', fromlist=[simulated])
 fake = getattr(dynamic_fake_provider, simulated)
 
+
+BASES = [('Z', 'Z'), ('Z', 'X'), ('X', 'Z'), ('X', 'X')]
+
+
 class ChshCircuit:
 
-    def __init__(self, num_pairs: int, token: str | None) -> None:
-        if num_pairs % 2:
-            raise ValueError("Number of qubits must be even")
+    def __init__(self, num_pairs: int, token: str | None, angle: float=math.pi/4) -> None:
         if token:
             service = QiskitRuntimeService(channel="ibm_quantum", token=token)
             self._backend = service.least_busy(operational=True, simulator=False, min_num_qubits=num_pairs * 2)
             self._sampler = SamplerV2(mode=self._backend)
             self._simulated = False
         else:
-            print("Using simulated circuit")
+            print(f"Using simulated circuit ('{simulated}')")
             self._backend = fake()
             self._sampler = AerSimulator.from_backend(self._backend)
             self._simulated = True
         self._estimator = EstimatorV2(mode=self._backend)
         self.num_qubits = num_pairs * 2
+        self._angle = angle
+        self._measurements: dict[int, dict[tuple, int]] = {i: {basis: 0 for basis in BASES} for i in range(num_pairs)}
+        self._num_measurements: dict[tuple, int] = {basis: 0 for basis in BASES}
+
+    
+    def _generate_entangled_qubits(self) -> QuantumCircuit:
+        circuit = QuantumCircuit(self.num_qubits)
+        circuit.h(range(0, self.num_qubits, 2))
+        circuit.cx(range(0, self.num_qubits, 2), range(1, self.num_qubits, 2))
+        circuit.ry(self._angle, range(0, self.num_qubits, 2))
+        return circuit
 
 
-    def generate_numbers(self) -> str:
-        # Prepare Circuit
-        circuit = _generate_entangled_qubits(self.num_qubits)
+    def generate_numbers(self, num_shots: int) -> str:
+        if not num_shots:
+            return ''
+        circuit = self._generate_entangled_qubits()
         circuit.measure_all()
         pass_manager = generate_preset_pass_manager(backend=self._backend, optimization_level=1)
         circuit = pass_manager.run(circuit)
 
-        # Run Circuit
-        result = self._sampler.run([circuit], shots=1).result()
+        result = self._sampler.run([circuit], shots=num_shots).result()
         if self._simulated:
             counts = result.get_counts(0)
-            bits = list(counts.keys())[0]
+            bits = ''.join(counts.keys())
         else:
-            bitstrings = np.array(result[0].data.meas.get_bitstrings())
+            bitstrings = result[0].data.meas.get_bitstrings()
             bits =  ''.join(bitstrings)
 
         return bits[::2]
 
 
-    def check_chsh(self, basis: tuple) -> list[float]:
-        circuit = _generate_entangled_qubits(self.num_qubits)
-        pm = generate_preset_pass_manager(target=self._backend.target, optimization_level=3)
-        circuit: QuantumCircuit = pm.run(circuit)
+    def check_measurement(self, basis: tuple, num_shots: int) -> None:
+        if not num_shots:
+            return
+        circuit = self._generate_entangled_qubits()
+        A, B = basis
+        if A == 'X':
+            circuit.h(range(0, self.num_qubits, 2))
+        if B == 'X':
+            circuit.h(range(1, self.num_qubits, 2))
+        circuit.measure_all()
+        pass_manager = generate_preset_pass_manager(target=self._backend.target, optimization_level=3)
+        circuit: QuantumCircuit = pass_manager.run(circuit)
         
-        observables = []
-        for i in range(0, self.num_qubits, 2):
-            observable = self._chsh_operator(basis, i)
-            observable = observable.apply_layout(circuit.layout)
-            observables.append(observable)
+        result = self._sampler.run([circuit], shots=num_shots).result()
+        if self._simulated:
+            counts = result.get_counts()
+        else:
+            bitstrings = result[0].data.meas.get_bitstrings()
+            counts= dict(Counter(bitstrings))
+        
+        for key in self._measurements.keys():
+            for measurement, count in counts.items():
+                pair_measurement = measurement[2*key:2*key+2]
+                if pair_measurement[0] == pair_measurement[1]:
+                    self._measurements[key][basis] += count
+                else:
+                    self._measurements[key][basis] -= count
 
-        pub = [(
-            circuit,
-            observables,
-        )]
+        self._num_measurements[basis] += num_shots
 
-        result = self._estimator.run(pubs=pub).result()
-        return [float(v) for v in result[0].data.evs]
+    
+    def get_violations(self) -> list[float]:
+        return [
+            (
+                (self._measurements[i]['Z', 'Z'] / self._num_measurements['Z', 'Z'])
+                + (self._measurements[i]['X', 'Z'] / self._num_measurements['X', 'Z']) 
+                - (self._measurements[i]['Z', 'X'] / self._num_measurements['Z', 'X'])
+                + (self._measurements[i]['X', 'X'] / self._num_measurements['X', 'X']) 
+            )
+            for i in range(self.num_qubits // 2)
+        ]
 
     def _chsh_operator(self, base, i):
         A, B = base
@@ -80,11 +116,3 @@ class ChshCircuit:
                 (_pad(B, B), 1)
             ]
         )
-
-
-def _generate_entangled_qubits(num_qubits: int) -> QuantumCircuit:
-    circuit = QuantumCircuit(num_qubits)
-    circuit.h(range(0, num_qubits, 2))
-    circuit.cx(range(0, num_qubits, 2), range(1, num_qubits, 2))
-    circuit.ry(math.pi / 4, range(0, num_qubits, 2))
-    return circuit
--- quant_num_generator/diqrng/generate.py
+++ quant_num_generator/diqrng/generate.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import math
+from itertools import cycle
 
 from diqrng import circuits
 
@@ -16,27 +17,71 @@ class Round(Enum):
     CHECK = 1
 
 
-def generate(rounds: list[int], qubits: int, token: str | None, bases: list[tuple] | None = None, uncertainty=0.20):
+def generate(
+        rounds: list[int],
+        num_pairs: int,
+        token: str | None,
+        bases: list[tuple] | None,
+        gen_shots: int=1,
+        check_shots=10,
+        uncertainty=0.20,
+        angle=math.pi/4
+    ) -> str:
+    """
+    Generate random numbers using the CHSH protocol. The number of random numbers generated will \
+        be the product of the number of pairs, the number of shots, and the number of GENERATE \
+        rounds.
+    
+    Parameters:
+        rounds (list[int]): A list of integers representing the rounds to run. 0 for GENERATE, 1 \
+            for CHECK.
+        num_pairs (int): The number of pairs of entangled qubits to use.
+        token (str | None): The IBM Quantum token to use. If None, a simulated circuit will be \
+            used.   
+        bases (list[tuple] | None): A list of tuples representing the bases to use for each round.\
+             If None, the bases will cycle through the default bases. Bases should be 2-tuple of \
+            strings, either 'Z' or 'X'.
+        gen_shots (int): The number of shots to use for the GENERATE round.
+        check_shots (int): The number of shots to use for the CHECK round.
+        uncertainty (float): The uncertainty to allow in the CHSH violations.
+        angle (float): The angle to use for the entangled qubits. Maximal CHSH violations are \
+            produced when angle = pi/4.
+    
+    Returns:
+        str: A string of random numbers generated using the CHSH protocol.
+
+    Raises:
+        AbortGeneration: If the CHSH violations do not satisfy the threshold.
+    """
     random_numbers = ""
-    threshold = _get_threshold(uncertainty)
-    if bases:
-        raise NotImplementedError("Custom bases are not supported.")
-    chsh_circuit = circuits.ChshCircuit(qubits, token)
+    threshold = get_threshold(uncertainty)
+    iter_bases = iter(bases) if bases else cycle([('Z', 'Z'), ('Z', 'X'), ('X', 'Z'), ('X', 'X')])
+    chsh_circuit = circuits.ChshCircuit(num_pairs, token, angle)
+    num_gens = 0
 
     for round in rounds:
         if Round(round) == Round.GENERATE:
-            random_numbers += chsh_circuit.generate_numbers()
+            num_gens += 1
         elif Round(round) == Round.CHECK:
-            violations = chsh_circuit.check_chsh(('Z', 'X'))
-            failures = [v for v in violations if v < threshold]
-            if failures:
-                raise AbortGeneration(failures, threshold)
+            random_numbers += chsh_circuit.generate_numbers(num_shots=gen_shots * num_gens)
+            num_gens = 0
+            chsh_circuit.check_measurement(next(iter_bases), num_shots=check_shots)
         else:
             raise ValueError(f"Round type not recognised: {round}")
+    
+    violations = chsh_circuit.get_violations()
+    print("Violations")
+    print("==========")
+    print(violations)
+    failures = [v for v in violations if v < threshold]
+    if failures:
+        raise AbortGeneration(failures, threshold)
+
+    # Generate any remaining random numbers
+    random_numbers += chsh_circuit.generate_numbers(num_shots=gen_shots * num_gens)
     return random_numbers
 
-
-def _get_threshold(uncertainty: float) -> float:
+def get_threshold(uncertainty: float) -> float:
     if uncertainty > 1:
         raise ValueError("Uncertainty cannot be greater than 1")
     return 2 * (math.sqrt(2) - uncertainty * (math.sqrt(2) - 1))
